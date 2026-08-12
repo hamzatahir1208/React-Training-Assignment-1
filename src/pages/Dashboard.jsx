6aa6273f9e09f1c1cb46cb92be2d5db39f514205
hamzatahir1208 <hamza.tahir1208@hazelsoft.org>
2026-08-12 14:10:42 +0500

    Add users, Delete users logic

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,63 @@
 import Layout from "../layouts/Layout";
 import Table from "../components/Table";
+import UserModal from "../components/UserModal";
+
+import { getUsers } from "../services/userService";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const loadUsers = async () => {
+      const data = await getUsers();
+
+      setUsers(data);
+    };
+
+    loadUsers();
+  }, []);
+
+  const handleAddUser = (newUser) => {
+
+    const user = {
+      id: Date.now(),
+      ...newUser
+    };
+
+    setUsers((currentUsers) => [
+      ...currentUsers,
+      user
+    ]);
+  };
+
+
+  const handleDeleteUser = (id) => {
+    setUsers((currentUsers) => {
+      return currentUsers.filter((user) => user.id != id);
+    });
+  };
+
   return (
     <Layout>
       <div className="container ">
         <div className="content mt-5 p-3">
           <h1>Dashboard</h1>
+          <button
+            type="button"
+            className="btn "
+            data-bs-toggle="modal"
+            data-bs-target="#userModal"
+          >
+            <i className="bi bi-plus-circle text-light fs-2"></i>
+          </button>
         </div>
-        <div className="table mt-5">
-          <Table data={users}/>
+        <div className="mt-5">
+          <Table data={users} handelDelete={handleDeleteUser} />
         </div>
       </div>
+
+      <UserModal onAdd={handleAddUser}/>
     </Layout>
   );
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,3 +3,9 @@ import users from '../data/users.json';
 export const getUsers = async () => {
     return users;
 }
+
+export const addUser = async (user) => {
+};
+
+export const deleteUser = async (id) => {
+};
